444aea0ca1c55119c769fe9fb9a62ce36000fa50
Ivo Reich <ir-dev@reich.onl>
2023-04-04 01:15:26 +0200

    Generator with common GOL patterns and refactorings

--- cellMapPreset.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class CellMapPreset(Enum):
-    EMPTY = 0
-    RANDOM = 1
-    # TODO: (OPTIONAL) Add more presets (e.g. glider, glider gun, etc.
-    # GLIDER = 2
-    # GLIDER_GUN = 3
--- gameOfLifeApp.py
+++ gameOfLifeApp.py
@@ -1,13 +1,12 @@
 from enum import Enum
-from os import urandom
-import math
+import random
 import pygame
 
-from cellMapPreset import CellMapPreset
+from cellMapGenerator import CellMapGenerator, CellMapPreset
 
 
 WINDOW_INITIAL_SIZE = (1280, 720)
-SIMULATION_INITIAL_SPEED = 10
+SIMULATION_INITIAL_SPEED = 30
 SIMULATION_MIN_SPEED = 1
 SIMULATION_MAX_SPEED = 1000
 FPS = 60.0
@@ -32,11 +31,29 @@ class GameOfLifeApp:
         self.simulation_speed = SIMULATION_INITIAL_SPEED
         self.reset()
 
+    def get_available_cell_map_cell_nums(self):
+        window_size = self.window_surface.get_size()
+        window_width, window_height = window_size
+        cells_num_x = window_width // CELL_SIZE
+        cells_num_y = window_height // CELL_SIZE
+        return cells_num_x, cells_num_y
+
+    @staticmethod
+    def get_cell_map_cell_nums_static(cell_map):
+        return len(cell_map[0]), len(cell_map)
+
+    def get_cell_map_cell_nums(self):
+        return len(self.cell_map[0]), len(self.cell_map)
+
+    def get_cell_map_size(self):
+        return len(self.cell_map[0]) * CELL_SIZE, len(self.cell_map) * CELL_SIZE
+
     def reset(self, initial_configuration=None):
         # Use initial_configuration if provided, otherwise use self.initial_configuration if set, otherwise use default
         if initial_configuration is None:
             if not hasattr(self, 'initial_configuration') or self.initial_configuration is None:
-                initial_configuration = self.get_cell_map(CELL_MAP_PRESET_DEFAULT)
+                initial_configuration = CellMapGenerator.get_cell_map(
+                    self.get_available_cell_map_cell_nums(), CELL_MAP_PRESET_DEFAULT)
             else:
                 initial_configuration = self.initial_configuration
 
@@ -46,52 +63,10 @@ class GameOfLifeApp:
         self.cell_map_surface = None
         self.simulation_step = 0
 
-    def get_cell_map(self, cell_map_preset: CellMapPreset = CellMapPreset.EMPTY):
-        window_size = self.window_surface.get_size()
-        window_width, window_height = window_size
-        cells_num_x = window_width // CELL_SIZE
-        cells_num_y = window_height // CELL_SIZE
-        range_x = range(cells_num_x)
-        range_y = range(cells_num_y)
-        match cell_map_preset:
-            case CellMapPreset.EMPTY:
-                cell_map = [[0 for x in range_x] for y in range_y]
-            case CellMapPreset.RANDOM:
-                required_random_bytes = math.ceil((cells_num_x * cells_num_y) / 8)
-                random_bits = [(b >> i) & 1 for b in urandom(required_random_bytes) for i in range(8)]
-                cell_map = [[random_bits[y * cells_num_x + x] for x in range_x] for y in range_y]
-        return cell_map
-
-    def apply_grid_scaling(self):
-        window_size = self.window_surface.get_size()
-        window_width, window_height = window_size
-        cells_num_x = window_width // CELL_SIZE
-        cells_num_y = window_height // CELL_SIZE
-        actual_cells_num_x = len(self.cell_map[0])
-        actual_cells_num_y = len(self.cell_map)
-        if cells_num_x != actual_cells_num_x or cells_num_y != actual_cells_num_y:
-            # scale the grid depending on the delta of cell nums (delta < 0: remove rows/columns, delta > 0: add rows/columns)
-            cells_delta_x = cells_num_x - actual_cells_num_x
-            cells_delta_y = cells_num_y - actual_cells_num_y
-            if cells_delta_x < 0:
-                for row in self.cell_map:
-                    for _ in range(cells_delta_x * -1):
-                        row.pop()
-            elif cells_delta_x > 0:
-                for row in self.cell_map:
-                    for _ in range(cells_delta_x):
-                        row.append(0)
-            if cells_delta_y < 0:
-                for _ in range(cells_delta_y * -1):
-                    self.cell_map.pop()
-            elif cells_delta_y > 0:
-                for _ in range(cells_delta_y):
-                    self.cell_map.append([0 for _ in range(cells_num_x)])
-            self.reset(self.cell_map)
-
     def process_events(self):
         def is_cell_collision(point):
-            cell_map_rect = pygame.Rect(0, 0, len(self.cell_map[0]) * CELL_SIZE, len(self.cell_map) * CELL_SIZE)
+            cell_map_width, cell_map_height = self.get_cell_map_size()
+            cell_map_rect = pygame.Rect(0, 0, cell_map_width, cell_map_height)
             return cell_map_rect.collidepoint(point)
 
         def is_cell_modified(point):
@@ -102,8 +77,7 @@ class GameOfLifeApp:
                 return
 
             # edge case handling (when mouse is at the right or bottom edge of the cell map)
-            cell_map_width = len(self.cell_map[0]) * CELL_SIZE
-            cell_map_height = len(self.cell_map) * CELL_SIZE
+            cell_map_width, cell_map_height = self.get_cell_map_size()
             if mx == cell_map_width:
                 x = mx // CELL_SIZE - 1
             else:
@@ -132,13 +106,23 @@ class GameOfLifeApp:
                         # clear
                         case pygame.K_c:
                             self.paused = True
-                            self.reset(self.get_cell_map(CellMapPreset.EMPTY))
+                            cell_map = CellMapGenerator.get_cell_map(
+                                self.get_available_cell_map_cell_nums(), CellMapPreset.EMPTY)
+                            self.reset(cell_map)
                         # reset
                         case pygame.K_r:
                             self.reset()
                         # new
                         case pygame.K_n:
-                            self.reset(self.get_cell_map(CellMapPreset.RANDOM))
+                            cell_map = CellMapGenerator.get_cell_map(
+                                self.get_available_cell_map_cell_nums(), CellMapPreset.RANDOM)
+                            self.reset(cell_map)
+                        # new (with pattern)
+                        case pygame.K_p:
+                            cell_map_pattern = CellMapPreset(random.randint(2, len(CellMapPreset) - 1))
+                            cell_map = CellMapGenerator.get_cell_map(
+                                self.get_available_cell_map_cell_nums(), cell_map_pattern)
+                            self.reset(cell_map)
                         # save
                         case pygame.K_s:
                             # TODO: save game state and use appropriate data structure
@@ -169,10 +153,32 @@ class GameOfLifeApp:
                             mx, my = event.pos
                             toggle_cell_at(mx, my)
 
+    def apply_grid_scaling(self):
+        available_cells_num_x, available_cells_num_y = self.get_available_cell_map_cell_nums()
+        cells_num_x, cells_num_y = self.get_cell_map_cell_nums()
+        if available_cells_num_x != cells_num_x or available_cells_num_y != cells_num_y:
+            # scale the grid depending on the delta of cell nums (delta < 0: remove rows/columns, delta > 0: add rows/columns)
+            cells_delta_x = available_cells_num_x - cells_num_x
+            cells_delta_y = available_cells_num_y - cells_num_y
+            if cells_delta_x < 0:
+                for row in self.cell_map:
+                    for _ in range(cells_delta_x * -1):
+                        row.pop()
+            elif cells_delta_x > 0:
+                for row in self.cell_map:
+                    for _ in range(cells_delta_x):
+                        row.append(0)
+            if cells_delta_y < 0:
+                for _ in range(cells_delta_y * -1):
+                    self.cell_map.pop()
+            elif cells_delta_y > 0:
+                for _ in range(cells_delta_y):
+                    self.cell_map.append([0 for _ in range(available_cells_num_x)])
+            self.reset(self.cell_map)
+
     @ staticmethod
     def simulate_map(cell_map):
-        cells_num_x = len(cell_map[0])
-        cells_num_y = len(cell_map)
+        cells_num_x, cells_num_y = GameOfLifeApp.get_cell_map_cell_nums_static(cell_map)
 
         last_row = [0] * cells_num_x
         curr_row = [0] * cells_num_x
@@ -217,8 +223,7 @@ class GameOfLifeApp:
 
     @ staticmethod
     def get_cell_map_surface(cell_map, cell_size):
-        cells_num_x = len(cell_map[0])
-        cells_num_y = len(cell_map)
+        cells_num_x, cells_num_y = GameOfLifeApp.get_cell_map_cell_nums_static(cell_map)
         color_type = GameOfLifeApp.Color
         cell_map_surface = pygame.Surface((cells_num_x * cell_size, cells_num_y * cell_size))
         cell_dead_surface = pygame.Surface((cell_size, cell_size))
@@ -272,8 +277,8 @@ class GameOfLifeApp:
 
             paused = "PAUSED " if self.paused else ""
             fps = self.clock.get_fps()
-            status_text = "{}Steps: {} Speed: {} FPS: {:.0f}".format(
-                paused, self.simulation_step, self.simulation_speed, fps)
+            status_text = "{}FPS: {:.0f} Speed: {} Steps: {}".format(
+                paused, fps, self.simulation_speed, self.simulation_step)
             status_text_surface = self.font.render(status_text, True, self.Color.WHITE.value)
             self.window_surface.blit(status_text_surface, (0, window_height - status_text_surface.get_height()))
 
